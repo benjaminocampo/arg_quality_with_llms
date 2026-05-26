75157df56bbb7d11651c90914975412102c618cc
Nicolás Benjamín Ocampo <nicolasbenjaminocampo@gmail.com>
2026-05-26 13:17:08 +0200

    Add all runs

--- experiments/run_all.sh
+++ experiments/run_all.sh
@@ -1,3 +1,154 @@
+bash run_job.sh conf/llm/mistral-7B-small.yaml conf/dim/dialectic.yaml conf/prompt/zero_shot.yaml conf/run/zero.yaml
+bash run_job.sh conf/llm/mistral-7B-small.yaml conf/dim/logic.yaml conf/prompt/zero_shot.yaml conf/run/zero.yaml
+bash run_job.sh conf/llm/mistral-7B-small.yaml conf/dim/rhetoric.yaml conf/prompt/zero_shot.yaml conf/run/zero.yaml
+
+bash run_job.sh conf/llm/mistral-7B-small.yaml conf/dim/dialectic.yaml conf/prompt/zero_shot.yaml conf/run/one.yaml
+bash run_job.sh conf/llm/mistral-7B-small.yaml conf/dim/logic.yaml conf/prompt/zero_shot.yaml conf/run/one.yaml
+bash run_job.sh conf/llm/mistral-7B-small.yaml conf/dim/rhetoric.yaml conf/prompt/zero_shot.yaml conf/run/one.yaml
+
+bash run_job.sh conf/llm/mistral-7B-small.yaml conf/dim/dialectic.yaml conf/prompt/zero_shot.yaml conf/run/two.yaml
+bash run_job.sh conf/llm/mistral-7B-small.yaml conf/dim/logic.yaml conf/prompt/zero_shot.yaml conf/run/two.yaml
+bash run_job.sh conf/llm/mistral-7B-small.yaml conf/dim/rhetoric.yaml conf/prompt/zero_shot.yaml conf/run/two.yaml
+
+
 bash run_job.sh conf/llm/llama-8B-small.yaml conf/dim/dialectic.yaml conf/prompt/zero_shot.yaml conf/run/zero.yaml
 bash run_job.sh conf/llm/llama-8B-small.yaml conf/dim/logic.yaml conf/prompt/zero_shot.yaml conf/run/zero.yaml
 bash run_job.sh conf/llm/llama-8B-small.yaml conf/dim/rhetoric.yaml conf/prompt/zero_shot.yaml conf/run/zero.yaml
+
+bash run_job.sh conf/llm/llama-8B-small.yaml conf/dim/dialectic.yaml conf/prompt/zero_shot.yaml conf/run/one.yaml
+bash run_job.sh conf/llm/llama-8B-small.yaml conf/dim/logic.yaml conf/prompt/zero_shot.yaml conf/run/one.yaml
+bash run_job.sh conf/llm/llama-8B-small.yaml conf/dim/rhetoric.yaml conf/prompt/zero_shot.yaml conf/run/one.yaml
+
+bash run_job.sh conf/llm/llama-8B-small.yaml conf/dim/dialectic.yaml conf/prompt/zero_shot.yaml conf/run/two.yaml
+bash run_job.sh conf/llm/llama-8B-small.yaml conf/dim/logic.yaml conf/prompt/zero_shot.yaml conf/run/two.yaml
+bash run_job.sh conf/llm/llama-8B-small.yaml conf/dim/rhetoric.yaml conf/prompt/zero_shot.yaml conf/run/two.yaml
+
+
+bash run_job.sh conf/llm/olmo2-7B-small.yaml conf/dim/dialectic.yaml conf/prompt/zero_shot.yaml conf/run/zero.yaml
+bash run_job.sh conf/llm/olmo2-7B-small.yaml conf/dim/logic.yaml conf/prompt/zero_shot.yaml conf/run/zero.yaml
+bash run_job.sh conf/llm/olmo2-7B-small.yaml conf/dim/rhetoric.yaml conf/prompt/zero_shot.yaml conf/run/zero.yaml
+
+bash run_job.sh conf/llm/olmo2-7B-small.yaml conf/dim/dialectic.yaml conf/prompt/zero_shot.yaml conf/run/one.yaml
+bash run_job.sh conf/llm/olmo2-7B-small.yaml conf/dim/logic.yaml conf/prompt/zero_shot.yaml conf/run/one.yaml
+bash run_job.sh conf/llm/olmo2-7B-small.yaml conf/dim/rhetoric.yaml conf/prompt/zero_shot.yaml conf/run/one.yaml
+
+bash run_job.sh conf/llm/olmo2-7B-small.yaml conf/dim/dialectic.yaml conf/prompt/zero_shot.yaml conf/run/two.yaml
+bash run_job.sh conf/llm/olmo2-7B-small.yaml conf/dim/logic.yaml conf/prompt/zero_shot.yaml conf/run/two.yaml
+bash run_job.sh conf/llm/olmo2-7B-small.yaml conf/dim/rhetoric.yaml conf/prompt/zero_shot.yaml conf/run/two.yaml
+
+
+bash run_job.sh conf/llm/qwen2.5-7B-small.yaml conf/dim/dialectic.yaml conf/prompt/zero_shot.yaml conf/run/zero.yaml
+bash run_job.sh conf/llm/qwen2.5-7B-small.yaml conf/dim/logic.yaml conf/prompt/zero_shot.yaml conf/run/zero.yaml
+bash run_job.sh conf/llm/qwen2.5-7B-small.yaml conf/dim/rhetoric.yaml conf/prompt/zero_shot.yaml conf/run/zero.yaml
+
+bash run_job.sh conf/llm/qwen2.5-7B-small.yaml conf/dim/dialectic.yaml conf/prompt/zero_shot.yaml conf/run/one.yaml
+bash run_job.sh conf/llm/qwen2.5-7B-small.yaml conf/dim/logic.yaml conf/prompt/zero_shot.yaml conf/run/one.yaml
+bash run_job.sh conf/llm/qwen2.5-7B-small.yaml conf/dim/rhetoric.yaml conf/prompt/zero_shot.yaml conf/run/one.yaml
+
+bash run_job.sh conf/llm/qwen2.5-7B-small.yaml conf/dim/dialectic.yaml conf/prompt/zero_shot.yaml conf/run/two.yaml
+bash run_job.sh conf/llm/qwen2.5-7B-small.yaml conf/dim/logic.yaml conf/prompt/zero_shot.yaml conf/run/two.yaml
+bash run_job.sh conf/llm/qwen2.5-7B-small.yaml conf/dim/rhetoric.yaml conf/prompt/zero_shot.yaml conf/run/two.yaml
+
+
+bash run_job.sh conf/llm/commandr-7B-small.yaml conf/dim/dialectic.yaml conf/prompt/zero_shot.yaml conf/run/zero.yaml
+bash run_job.sh conf/llm/commandr-7B-small.yaml conf/dim/logic.yaml conf/prompt/zero_shot.yaml conf/run/zero.yaml
+bash run_job.sh conf/llm/commandr-7B-small.yaml conf/dim/rhetoric.yaml conf/prompt/zero_shot.yaml conf/run/zero.yaml
+
+bash run_job.sh conf/llm/commandr-7B-small.yaml conf/dim/dialectic.yaml conf/prompt/zero_shot.yaml conf/run/one.yaml
+bash run_job.sh conf/llm/commandr-7B-small.yaml conf/dim/logic.yaml conf/prompt/zero_shot.yaml conf/run/one.yaml
+bash run_job.sh conf/llm/commandr-7B-small.yaml conf/dim/rhetoric.yaml conf/prompt/zero_shot.yaml conf/run/one.yaml
+
+bash run_job.sh conf/llm/commandr-7B-small.yaml conf/dim/dialectic.yaml conf/prompt/zero_shot.yaml conf/run/two.yaml
+bash run_job.sh conf/llm/commandr-7B-small.yaml conf/dim/logic.yaml conf/prompt/zero_shot.yaml conf/run/two.yaml
+bash run_job.sh conf/llm/commandr-7B-small.yaml conf/dim/rhetoric.yaml conf/prompt/zero_shot.yaml conf/run/two.yaml
+
+
+bash run_job.sh conf/llm/mixtral-8x7B-medium.yaml conf/dim/dialectic.yaml conf/prompt/zero_shot.yaml conf/run/zero.yaml
+bash run_job.sh conf/llm/mixtral-8x7B-medium.yaml conf/dim/logic.yaml conf/prompt/zero_shot.yaml conf/run/zero.yaml
+bash run_job.sh conf/llm/mixtral-8x7B-medium.yaml conf/dim/rhetoric.yaml conf/prompt/zero_shot.yaml conf/run/zero.yaml
+
+bash run_job.sh conf/llm/mixtral-8x7B-medium.yaml conf/dim/dialectic.yaml conf/prompt/zero_shot.yaml conf/run/one.yaml
+bash run_job.sh conf/llm/mixtral-8x7B-medium.yaml conf/dim/logic.yaml conf/prompt/zero_shot.yaml conf/run/one.yaml
+bash run_job.sh conf/llm/mixtral-8x7B-medium.yaml conf/dim/rhetoric.yaml conf/prompt/zero_shot.yaml conf/run/one.yaml
+
+bash run_job.sh conf/llm/mixtral-8x7B-medium.yaml conf/dim/dialectic.yaml conf/prompt/zero_shot.yaml conf/run/two.yaml
+bash run_job.sh conf/llm/mixtral-8x7B-medium.yaml conf/dim/logic.yaml conf/prompt/zero_shot.yaml conf/run/two.yaml
+bash run_job.sh conf/llm/mixtral-8x7B-medium.yaml conf/dim/rhetoric.yaml conf/prompt/zero_shot.yaml conf/run/two.yaml
+
+
+bash run_job.sh conf/llm/mistral-22B-medium.yaml conf/dim/dialectic.yaml conf/prompt/zero_shot.yaml conf/run/zero.yaml
+bash run_job.sh conf/llm/mistral-22B-medium.yaml conf/dim/logic.yaml conf/prompt/zero_shot.yaml conf/run/zero.yaml
+bash run_job.sh conf/llm/mistral-22B-medium.yaml conf/dim/rhetoric.yaml conf/prompt/zero_shot.yaml conf/run/zero.yaml
+
+bash run_job.sh conf/llm/mistral-22B-medium.yaml conf/dim/dialectic.yaml conf/prompt/zero_shot.yaml conf/run/one.yaml
+bash run_job.sh conf/llm/mistral-22B-medium.yaml conf/dim/logic.yaml conf/prompt/zero_shot.yaml conf/run/one.yaml
+bash run_job.sh conf/llm/mistral-22B-medium.yaml conf/dim/rhetoric.yaml conf/prompt/zero_shot.yaml conf/run/one.yaml
+
+bash run_job.sh conf/llm/mistral-22B-medium.yaml conf/dim/dialectic.yaml conf/prompt/zero_shot.yaml conf/run/two.yaml
+bash run_job.sh conf/llm/mistral-22B-medium.yaml conf/dim/logic.yaml conf/prompt/zero_shot.yaml conf/run/two.yaml
+bash run_job.sh conf/llm/mistral-22B-medium.yaml conf/dim/rhetoric.yaml conf/prompt/zero_shot.yaml conf/run/two.yaml
+
+
+bash run_job.sh conf/llm/olmo2-32B-medium.yaml conf/dim/dialectic.yaml conf/prompt/zero_shot.yaml conf/run/zero.yaml
+bash run_job.sh conf/llm/olmo2-32B-medium.yaml conf/dim/logic.yaml conf/prompt/zero_shot.yaml conf/run/zero.yaml
+bash run_job.sh conf/llm/olmo2-32B-medium.yaml conf/dim/rhetoric.yaml conf/prompt/zero_shot.yaml conf/run/zero.yaml
+
+bash run_job.sh conf/llm/olmo2-32B-medium.yaml conf/dim/dialectic.yaml conf/prompt/zero_shot.yaml conf/run/one.yaml
+bash run_job.sh conf/llm/olmo2-32B-medium.yaml conf/dim/logic.yaml conf/prompt/zero_shot.yaml conf/run/one.yaml
+bash run_job.sh conf/llm/olmo2-32B-medium.yaml conf/dim/rhetoric.yaml conf/prompt/zero_shot.yaml conf/run/one.yaml
+
+bash run_job.sh conf/llm/olmo2-32B-medium.yaml conf/dim/dialectic.yaml conf/prompt/zero_shot.yaml conf/run/two.yaml
+bash run_job.sh conf/llm/olmo2-32B-medium.yaml conf/dim/logic.yaml conf/prompt/zero_shot.yaml conf/run/two.yaml
+bash run_job.sh conf/llm/olmo2-32B-medium.yaml conf/dim/rhetoric.yaml conf/prompt/zero_shot.yaml conf/run/two.yaml
+
+
+bash run_job.sh conf/llm/mixtral-8x22B-medium.yaml conf/dim/dialectic.yaml conf/prompt/zero_shot.yaml conf/run/zero.yaml
+bash run_job.sh conf/llm/mixtral-8x22B-medium.yaml conf/dim/logic.yaml conf/prompt/zero_shot.yaml conf/run/zero.yaml
+bash run_job.sh conf/llm/mixtral-8x22B-medium.yaml conf/dim/rhetoric.yaml conf/prompt/zero_shot.yaml conf/run/zero.yaml
+
+bash run_job.sh conf/llm/mixtral-8x22B-medium.yaml conf/dim/dialectic.yaml conf/prompt/zero_shot.yaml conf/run/one.yaml
+bash run_job.sh conf/llm/mixtral-8x22B-medium.yaml conf/dim/logic.yaml conf/prompt/zero_shot.yaml conf/run/one.yaml
+bash run_job.sh conf/llm/mixtral-8x22B-medium.yaml conf/dim/rhetoric.yaml conf/prompt/zero_shot.yaml conf/run/one.yaml
+
+bash run_job.sh conf/llm/mixtral-8x22B-medium.yaml conf/dim/dialectic.yaml conf/prompt/zero_shot.yaml conf/run/two.yaml
+bash run_job.sh conf/llm/mixtral-8x22B-medium.yaml conf/dim/logic.yaml conf/prompt/zero_shot.yaml conf/run/two.yaml
+bash run_job.sh conf/llm/mixtral-8x22B-medium.yaml conf/dim/rhetoric.yaml conf/prompt/zero_shot.yaml conf/run/two.yaml
+
+
+bash run_job.sh conf/llm/llama-70B-big.yaml conf/dim/dialectic.yaml conf/prompt/zero_shot.yaml conf/run/zero.yaml
+bash run_job.sh conf/llm/llama-70B-big.yaml conf/dim/logic.yaml conf/prompt/zero_shot.yaml conf/run/zero.yaml
+bash run_job.sh conf/llm/llama-70B-big.yaml conf/dim/rhetoric.yaml conf/prompt/zero_shot.yaml conf/run/zero.yaml
+
+bash run_job.sh conf/llm/llama-70B-big.yaml conf/dim/dialectic.yaml conf/prompt/zero_shot.yaml conf/run/one.yaml
+bash run_job.sh conf/llm/llama-70B-big.yaml conf/dim/logic.yaml conf/prompt/zero_shot.yaml conf/run/one.yaml
+bash run_job.sh conf/llm/llama-70B-big.yaml conf/dim/rhetoric.yaml conf/prompt/zero_shot.yaml conf/run/one.yaml
+
+bash run_job.sh conf/llm/llama-70B-big.yaml conf/dim/dialectic.yaml conf/prompt/zero_shot.yaml conf/run/two.yaml
+bash run_job.sh conf/llm/llama-70B-big.yaml conf/dim/logic.yaml conf/prompt/zero_shot.yaml conf/run/two.yaml
+bash run_job.sh conf/llm/llama-70B-big.yaml conf/dim/rhetoric.yaml conf/prompt/zero_shot.yaml conf/run/two.yaml
+
+
+bash run_job.sh conf/llm/qwen2.5-72B-big.yaml conf/dim/dialectic.yaml conf/prompt/zero_shot.yaml conf/run/zero.yaml
+bash run_job.sh conf/llm/qwen2.5-72B-big.yaml conf/dim/logic.yaml conf/prompt/zero_shot.yaml conf/run/zero.yaml
+bash run_job.sh conf/llm/qwen2.5-72B-big.yaml conf/dim/rhetoric.yaml conf/prompt/zero_shot.yaml conf/run/zero.yaml
+
+bash run_job.sh conf/llm/qwen2.5-72B-big.yaml conf/dim/dialectic.yaml conf/prompt/zero_shot.yaml conf/run/one.yaml
+bash run_job.sh conf/llm/qwen2.5-72B-big.yaml conf/dim/logic.yaml conf/prompt/zero_shot.yaml conf/run/one.yaml
+bash run_job.sh conf/llm/qwen2.5-72B-big.yaml conf/dim/rhetoric.yaml conf/prompt/zero_shot.yaml conf/run/one.yaml
+
+bash run_job.sh conf/llm/qwen2.5-72B-big.yaml conf/dim/dialectic.yaml conf/prompt/zero_shot.yaml conf/run/two.yaml
+bash run_job.sh conf/llm/qwen2.5-72B-big.yaml conf/dim/logic.yaml conf/prompt/zero_shot.yaml conf/run/two.yaml
+bash run_job.sh conf/llm/qwen2.5-72B-big.yaml conf/dim/rhetoric.yaml conf/prompt/zero_shot.yaml conf/run/two.yaml
+
+
+bash run_job.sh conf/llm/commandr-104B-big.yaml conf/dim/dialectic.yaml conf/prompt/zero_shot.yaml conf/run/zero.yaml
+bash run_job.sh conf/llm/commandr-104B-big.yaml conf/dim/logic.yaml conf/prompt/zero_shot.yaml conf/run/zero.yaml
+bash run_job.sh conf/llm/commandr-104B-big.yaml conf/dim/rhetoric.yaml conf/prompt/zero_shot.yaml conf/run/zero.yaml
+
+bash run_job.sh conf/llm/commandr-104B-big.yaml conf/dim/dialectic.yaml conf/prompt/zero_shot.yaml conf/run/one.yaml
+bash run_job.sh conf/llm/commandr-104B-big.yaml conf/dim/logic.yaml conf/prompt/zero_shot.yaml conf/run/one.yaml
+bash run_job.sh conf/llm/commandr-104B-big.yaml conf/dim/rhetoric.yaml conf/prompt/zero_shot.yaml conf/run/one.yaml
+
+bash run_job.sh conf/llm/commandr-104B-big.yaml conf/dim/dialectic.yaml conf/prompt/zero_shot.yaml conf/run/two.yaml
+bash run_job.sh conf/llm/commandr-104B-big.yaml conf/dim/logic.yaml conf/prompt/zero_shot.yaml conf/run/two.yaml
+bash run_job.sh conf/llm/commandr-104B-big.yaml conf/dim/rhetoric.yaml conf/prompt/zero_shot.yaml conf/run/two.yaml
